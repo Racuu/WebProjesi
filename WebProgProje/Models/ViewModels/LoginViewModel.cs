095ef2e0b5419c076531a9edb727c1ee9aa6e330
Racuu <63163802+Racuu@users.noreply.github.com>
2021-12-26 03:34:30 +0300

    initial commit

---
+++ WebProgProje/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProgProje.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+
+        [Required(ErrorMessage = "Lütfen kullanıcı adını boş geçmeyiniz.")]
+        [Display(Name = "Kullanıcı Adı ")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifreyi boş geçmeyiniz.")]
+        [DataType(DataType.Password, ErrorMessage = "Lütfen uygun formatta şifre giriniz.")]
+        [Display(Name = "Şifre")]
+        public string Password { get; set; }
+        /// <summary>
+        /// Beni hatırla...
+        /// </summary>
+        [Display(Name = "Beni Hatırla")]
+        public bool Persistent { get; set; }
+        public bool Lock { get; set; }
+    }
+}
